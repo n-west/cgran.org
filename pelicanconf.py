344f3edb86f353b62409fc703fd7da44b88f62fd
Ravi Sharan <ravisharan@iith.ac.in>
2015-01-20 11:38:43 +0530

    [TEST] Second push

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,5 +24,7 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME="home/ravi/sandbox/cgran-frontend/proto-pybombs"
+THEME= '/home/ravi/sandbox/cgran-frontend/proto-pybombs'
 
+DIRECT_TEMPLATES = ('index',)
+PAGE_SAVE_AS = ''
