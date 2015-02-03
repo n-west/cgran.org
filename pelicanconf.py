9302803ec253045840eb38c41ad1e2d889fedfb3
Nathan West <nathan.west@okstate.edu>
2015-02-03 11:42:37 -0600

    remove reference to ravis computer

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,7 +24,7 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME= '/home/ravi/sandbox/cgran-frontend/proto-pybombs'
+THEME= 'proto-pybombs'
 
 DIRECT_TEMPLATES = ('index', 'recipelist',)
 
