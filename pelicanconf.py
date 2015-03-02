0b61b053501c6bc1025dd8142ef607966393806c
Ravi Sharan <ravisharan@iith.ac.in>
2015-03-02 16:18:26 +0530

    [TEST]Change pelicanconf.py to build the site on Arch Linux

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,7 +26,12 @@ DEFAULT_PAGINATION = False
 
 THEME= 'proto-pybombs'
 
-DIRECT_TEMPLATES = ('index',)
-PAGE_PATHS = ['oot_modules']
 PLUGIN_PATHS = ['plugins/pelican-md-metayaml']
 PLUGINS = ['md_metayaml']
+DIRECT_TEMPLATES = ['index',]
+PAGE_PATHS = ['oot_modules']
+LOAD_CONTENT_CACHE=False
+ARTICLE_EXCLUDE=['oot_modules']
+#STATIC_PATHS = ["js"]
+
+## custom page generated with a jinja2 template
